122189949448e8e858cc26d85127d69691ae9563
jkim68888 <jkim68888@gmail.com>
2022-01-03 18:30:54 +0900

    문제 파일 미리 생성

---
+++ 2022-01-04-하샤드수.js
@@ -0,0 +1,12 @@
+// https://programmers.co.kr/learn/courses/30/lessons/12947
+// 자연수 x 가 하샤드 수 인지 아닌지 검사하는 함수를 만들어라.
+// 예시) 18의 자릿수 합은 1+8=9이고, 18은 9로 나누어 떨어지므로 18은 하샤드 수이다.
+
+function solution1(x) {
+  return
+}
+
+console.log('10은 하샤드수?', solution1(10))
+console.log('10은 하샤드수?', solution1(12))
+console.log('10은 하샤드수?', solution1(11))
+console.log('10은 하샤드수?', solution1(13))
